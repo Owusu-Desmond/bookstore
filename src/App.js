647044305f73232020a9c5cddc355ea62c55bb34
Owusu-Desmond <desmond0406114009@gmail.com>
2022-08-12 13:16:43 +0000

    Remove non-related info

--- src/App.js
+++ src/App.js
@@ -18,12 +18,6 @@ function App() {
             <Route path="/categories" element={<CategoriesPage />} />
           </Routes>
         </main>
-        <p>
-          Patupa is a simple and fast online shopping application that helps users can purchase with no
-          cost of delivery and short time delivery. Users can search and purchase any products of their choice
-          from this online shopping application. Users can also search for products of their choice.
-          Users can also add products to their cart and checkout.
-        </p>
       </div>
     </BrowserRouter>
   );
