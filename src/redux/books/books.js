dc5c9815814bc2ed71b7fdc193e4bb1a513e4237
Owusu-Desmond <desmond0406114009@gmail.com>
2022-08-16 13:33:34 +0000

    Create books for books as initial state

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,19 +1,45 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const books = [
+  {
+    id: uuidv4(),
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    id: uuidv4(),
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+  },
+  {
+    id: uuidv4(),
+    title: 'The Lord of the Rings',
+    author: 'J.R.R. Tolkien',
+  },
+  {
+    id: uuidv4(),
+    title: 'The Silmarillion',
+    author: 'J.R.R. Tolkien',
+  },
+];
 
-const bookReducer = (state = [], action) => {
+const bookReducer = (state = books, action) => {
+  const index = state.findIndex((book) => book.id === action.id);
+
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.book];
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.id);
+      return [...state.slice(0, index), ...state.slice(index + 1)];
     default:
       return state;
   }
 };
 
 const addBook = (book) => ({
-  type: 'ADD_BOOK',
+  type: ADD_BOOK,
   book,
 });
 
