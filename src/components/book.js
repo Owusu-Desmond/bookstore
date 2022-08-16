1d291d90f327ab114889d3ec52bcc7d86da69709
Owusu-Desmond <desmond0406114009@gmail.com>
2022-08-16 13:28:36 +0000

    remove book from redux store using hooks

--- src/components/book.js
+++ src/components/book.js
@@ -1,17 +1,25 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
 const Book = (props) => {
+  const dispatch = useDispatch();
   const { book } = props;
+  const handleRemove = () => { // remove book from store
+    dispatch(removeBook(book.id));
+  };
   return (
     <div className="book">
       <div className="book-title">{book.title}</div>
       <div className="book-author">{book.author}</div>
+      <button type="button" onClick={() => handleRemove(book.id)}>remove</button>
     </div>
   );
 };
 
 Book.propTypes = {
   book: PropTypes.shape({
+    id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
   }).isRequired,
